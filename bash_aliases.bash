24c28b816006a9d06cd894b4091ff9d772b96897
Vladimir Rodkin <mail@vovanr.com>
2020-12-02 14:20:51 +0300

    Aliases: Show human-friendly list of upgradable Apt

--- bash_aliases.bash
+++ bash_aliases.bash
@@ -41,6 +41,7 @@ alias bashaliases="$EDITOR ~/.bash_aliases"
 alias v-upgrade=" \
         echo -e \"== Apt ==\\n\" && \
         sudo apt update && \
+        apt list --upgradable
         sudo apt upgrade && \
         sudo apt dist-upgrade && \
         sudo apt autoremove && \
